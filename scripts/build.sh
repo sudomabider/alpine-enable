e22724d9b92d0301a0b9d4f61e65a607a7feeaf1
veo.chen <veo.chen@voyager.nz>
2019-05-03 15:45:27 +1200

    improve build dest

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,10 @@ export GOARCH=amd64
 tag=${CI_COMMIT_TAG:-unknown}
 buildtime=`date -u +%Y-%m-%d_%H\:%M\:%S`
 commithash=${CI_COMMIT_SHORT_SHA:-unknown}
-dest=$(dirname $0)
+dest=$(dirname $0)/../binaries
 
 echo using tag ${tag}
 echo using buildtime ${buildtime}
 echo using commithash ${commithash}
 
-go build --ldflags="-s -w -v -X main.version=${tag} -X main.buildTime=${buildtime} -X main.commitHash=${commithash}" -o ${dest}/../binaries/enable
+go build --ldflags="-s -w -v -X main.version=${tag} -X main.buildTime=${buildtime} -X main.commitHash=${commithash}" -o ${dest}/enable
